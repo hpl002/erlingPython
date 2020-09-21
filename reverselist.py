e901ede0ffe3af0e953fc2c934aa1ce678045c15
Herman Stoud Platou <hermanp@stacc.com>
2020-09-21 22:15:29 +0200

    init

---
+++ reverselist.py
@@ -0,0 +1,15 @@
+thislist = ["apple", "banana", "cherry"]
+#list in specified order
+print("original list ", thislist)
+print(thislist)
+#list now printed in revered order by use of 
+print("list with element orders reversed")
+print( thislist [::-1])
+ 
+# har ikke lest denne, men tilsynelatende god forklaring 
+#https://stackoverflow.com/questions/509211/understanding-slice-notation
+
+
+
+originalString = "herman"
+print( originalString [::-1])
